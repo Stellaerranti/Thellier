447d56bb92fd76899a3d32de2220a8a9123fc95b
Stellaerranti <ushakovda@my.msu.ru>
2025-11-11 19:14:08 +0800

    Basics done

---
+++ Thellier/Thellier/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Thellier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton_openPMD = new System.Windows.Forms.ToolStripButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ZiChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.demagChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ARMChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.MainTable = new System.Windows.Forms.DataGridView();
+            this.H = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Z = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NRM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ARMG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ARML = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ZiChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.demagChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ARMChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton_openPMD});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 4, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(2510, 42);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton_openPMD
+            // 
+            this.toolStripButton_openPMD.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton_openPMD.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_openPMD.Image")));
+            this.toolStripButton_openPMD.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_openPMD.Name = "toolStripButton_openPMD";
+            this.toolStripButton_openPMD.Size = new System.Drawing.Size(136, 36);
+            this.toolStripButton_openPMD.Text = "Open PMD";
+            this.toolStripButton_openPMD.Click += new System.EventHandler(this.toolStripButton_openPMD_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Controls.Add(this.ZiChart, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.demagChart, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ARMChart, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.MainTable, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 42);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(2510, 1204);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // ZiChart
+            // 
+            chartArea1.Name = "proj1";
+            chartArea2.Name = "proj2";
+            this.ZiChart.ChartAreas.Add(chartArea1);
+            this.ZiChart.ChartAreas.Add(chartArea2);
+            this.ZiChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ZiChart.Location = new System.Drawing.Point(6, 6);
+            this.ZiChart.Margin = new System.Windows.Forms.Padding(6);
+            this.ZiChart.Name = "ZiChart";
+            series1.ChartArea = "proj1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Name = "YX";
+            series2.ChartArea = "proj1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Name = "YmZ";
+            series3.ChartArea = "proj2";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Name = "YmX";
+            series4.ChartArea = "proj2";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Name = "ZX";
+            this.ZiChart.Series.Add(series1);
+            this.ZiChart.Series.Add(series2);
+            this.ZiChart.Series.Add(series3);
+            this.ZiChart.Series.Add(series4);
+            this.ZiChart.Size = new System.Drawing.Size(741, 590);
+            this.ZiChart.TabIndex = 0;
+            this.ZiChart.Text = "chart1";
+            // 
+            // demagChart
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.demagChart.ChartAreas.Add(chartArea3);
+            this.demagChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.demagChart.Location = new System.Drawing.Point(759, 6);
+            this.demagChart.Margin = new System.Windows.Forms.Padding(6);
+            this.demagChart.Name = "demagChart";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series5.Name = "Demag";
+            this.demagChart.Series.Add(series5);
+            this.demagChart.Size = new System.Drawing.Size(741, 590);
+            this.demagChart.TabIndex = 1;
+            this.demagChart.Text = "chart1";
+            // 
+            // ARMChart
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.ARMChart.ChartAreas.Add(chartArea4);
+            this.ARMChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.ARMChart.Legends.Add(legend1);
+            this.ARMChart.Location = new System.Drawing.Point(1512, 6);
+            this.ARMChart.Margin = new System.Windows.Forms.Padding(6);
+            this.ARMChart.Name = "ARMChart";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.ARMChart.Series.Add(series6);
+            this.ARMChart.Size = new System.Drawing.Size(741, 590);
+            this.ARMChart.TabIndex = 2;
+            this.ARMChart.Text = "chart1";
+            // 
+            // MainTable
+            // 
+            this.MainTable.AllowUserToAddRows = false;
+            this.MainTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MainTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.H,
+            this.X,
+            this.Y,
+            this.Z,
+            this.NRM,
+            this.ARMG,
+            this.ARML});
+            this.tableLayoutPanel1.SetColumnSpan(this.MainTable, 2);
+            this.MainTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainTable.Location = new System.Drawing.Point(6, 608);
+            this.MainTable.Margin = new System.Windows.Forms.Padding(6);
+            this.MainTable.Name = "MainTable";
+            this.MainTable.RowHeadersVisible = false;
+            this.MainTable.RowHeadersWidth = 82;
+            this.MainTable.Size = new System.Drawing.Size(1494, 590);
+            this.MainTable.TabIndex = 3;
+            // 
+            // H
+            // 
+            this.H.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.H.HeaderText = "H";
+            this.H.MinimumWidth = 10;
+            this.H.Name = "H";
+            // 
+            // X
+            // 
+            this.X.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.X.HeaderText = "X";
+            this.X.MinimumWidth = 10;
+            this.X.Name = "X";
+            // 
+            // Y
+            // 
+            this.Y.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Y.HeaderText = "Y";
+            this.Y.MinimumWidth = 10;
+            this.Y.Name = "Y";
+            // 
+            // Z
+            // 
+            this.Z.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Z.HeaderText = "Z";
+            this.Z.MinimumWidth = 10;
+            this.Z.Name = "Z";
+            // 
+            // NRM
+            // 
+            this.NRM.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.NRM.HeaderText = "NRM";
+            this.NRM.MinimumWidth = 10;
+            this.NRM.Name = "NRM";
+            // 
+            // ARMG
+            // 
+            this.ARMG.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ARMG.HeaderText = "ARM gained";
+            this.ARMG.MinimumWidth = 10;
+            this.ARMG.Name = "ARMG";
+            // 
+            // ARML
+            // 
+            this.ARML.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ARML.HeaderText = "AMR left";
+            this.ARML.MinimumWidth = 10;
+            this.ARML.Name = "ARML";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2510, 1246);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Thellier";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ZiChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.demagChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ARMChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ZiChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart demagChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ARMChart;
+        private System.Windows.Forms.DataGridView MainTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn H;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Z;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NRM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ARMG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ARML;
+        private System.Windows.Forms.ToolStripButton toolStripButton_openPMD;
+    }
+}
+
---
+++ Thellier/Thellier/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Thellier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ZiChart.ChartAreas["proj1"].Visible = false;
+        }
+
+        private (int,int) countDM(string[] lines)
+        {
+
+            int Lcount = 0;
+            int Lbegin = 0;
+
+            char[] mChar = {'M', 'm'};
+
+            foreach (string line in lines)
+            {
+
+                if (mChar.Contains(line[0]))
+                {                    
+                    if (line.Substring(1, 3) == "000" && Lcount > 0)
+                    {
+                        Lbegin = Lbegin + Lcount;
+                        Lcount = 0;
+                    }
+
+                    Lcount++;
+                }
+                else if(Lcount == 0) 
+                {
+                    Lbegin++;
+                }
+            }
+
+            return (Lcount,Lbegin);
+        }
+
+        private void plotPMD(double H, double X, double Y, double Z, double NRM)
+        {
+            demagChart.Series["Demag"].Points.AddXY(H, NRM);
+            ZiChart.Series["YX"].Points.AddXY(Y, X);
+            ZiChart.Series["YmZ"].Points.AddXY(Y, (-1)*Z);
+            ZiChart.Series["YmX"].Points.AddXY(Y, X);
+            ZiChart.Series["ZX"].Points.AddXY((-1) * Z,  X);
+        }
+
+        private void loadPMD(string path)
+        {
+            int ni = 0;
+
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(path);
+
+                (int MLines, int Lbegin) = countDM(lines);
+
+                NumberFormatInfo provider = new NumberFormatInfo();
+                provider.NumberDecimalSeparator = ".";
+
+                if (MainTable.RowCount != MLines && MainTable.RowCount > 0)
+                {
+                    MessageBox.Show("Different steps!");
+                    return;
+                }
+
+                if (MainTable.RowCount > 0)
+                {
+
+                }
+                else
+                {
+                    double H = 0, X = 0, Y = 0, Z = 0, NRM = 0;
+
+                    for (int i = 0; i < MLines; i++)
+                    {
+                        ni = i;
+
+                        while (lines[Lbegin + i].Contains("  ")) { lines[Lbegin + i] = lines[Lbegin + i].Replace("  ", " "); }
+                        while (lines[Lbegin + i].Contains("\t\t")) { lines[Lbegin + i] = lines[Lbegin + i].Replace("\t\t", "\t"); }
+                        while (lines[Lbegin + i].Contains("\t ")) { lines[Lbegin + i] = lines[Lbegin + i].Replace("\t ", "\t"); }
+                        while (lines[Lbegin + i].Contains(" \t")) { lines[Lbegin + i] = lines[Lbegin + i].Replace(" \t", "\t"); }
+                        while (lines[Lbegin + i].Contains(",")) { lines[Lbegin + i] = lines[Lbegin + i].Replace(",", "."); }
+
+                        string[] line = lines[Lbegin + i].Split(new[] { ' ', '\t' });
+
+                        H = double.Parse(line[0].Substring(1), System.Globalization.NumberStyles.Float, provider);
+                        X = double.Parse(line[1], System.Globalization.NumberStyles.Float, provider);
+                        Y = double.Parse(line[2], System.Globalization.NumberStyles.Float, provider);
+                        Z = double.Parse(line[3], System.Globalization.NumberStyles.Float, provider);
+                        NRM = double.Parse(line[4], System.Globalization.NumberStyles.Float, provider);
+
+                        MainTable.Rows.Add(H,X,Y,Z,NRM,0,0);
+                        plotPMD(H, X, Y, Z, NRM);
+
+                    }
+                }
+
+            }
+            catch 
+            {
+                MessageBox.Show("Error while reading file at line " + (ni + 1).ToString());
+            }
+        }
+
+        //double yr1 = Double.Parse(yr1Text.Text, NumberStyles.Any, CultureInfo.InvariantCulture);
+
+        /*
+         private void toolStripButton_LockationFile_Click(object sender, EventArgs e)
+        {
+            using (var fileDialog = new OpenFileDialog())
+            {
+
+                fileDialog.Title = "Select a File with coordinates";
+                fileDialog.Filter = "XML Files (*.xml)|*.xml";
+
+
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    coordFilePath = fileDialog.FileName;
+                    ReadXML(coordFilePath);
+                    CheckIfDataReady();
+                }
+            }
+        }
+         */
+
+        /*
+         private void readTestFile()
+        {
+            if (ImportTest.ShowDialog() == DialogResult.Cancel) { return; }
+
+            string filename = ImportTest.FileName;
+            int ni = 0;
+
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(filename);
+
+                TestFileLabel.Text = Path.GetFileName(filename);
+                double U = 0;
+                double Ca = 0;
+                double U_std = 0;
+                double Ca_std = 0;
+
+                double UCa = 0;
+                double UCa_std = 0;
+
+                NumberFormatInfo provider = new NumberFormatInfo();
+                provider.NumberDecimalSeparator = ".";
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    ni = i;
+
+                    if (lines[i].Length > 2)
+                    {
+                        while (lines[i].Contains("  ")) { lines[i] = lines[i].Replace("  ", " "); }
+                        while (lines[i].Contains("\t\t")) { lines[i] = lines[i].Replace("\t\t", "\t"); }
+                        while (lines[i].Contains("\t ")) { lines[i] = lines[i].Replace("\t ", "\t"); }
+                        while (lines[i].Contains(" \t")) { lines[i] = lines[i].Replace(" \t", "\t"); }
+                        while (lines[i].Contains(",")) { lines[i] = lines[i].Replace(",", "."); }
+
+                        string[] line = lines[i].Split(new[] { ' ', '\t' });
+
+                        U = Convert.ToDouble(line[1], provider);
+                        Ca = Convert.ToDouble(line[3], provider);
+
+                        U_std = Convert.ToDouble(line[2], provider);
+                        Ca_std = Convert.ToDouble(line[4], provider);
+
+                        UCa = U / Ca;
+
+                        //UCa_std = (Calc_UCa_std(U,Ca,U_std,Ca_std)/UCa)*(UCa* 1000000);
+                        UCa_std = Calc_UCa_std(U, Ca, U_std, Ca_std);
+
+                        TestGrid.Rows.Add((i+1).ToString("F0"), 0,U.ToString("F4"), U_std.ToString("F4"),Ca.ToString("E3"),Ca_std.ToString("E3"),UCa.ToString("E3"),UCa_std.ToString("E3"),0,0);
+                    }
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("Error while reading file at line " + (ni + 1).ToString());
+            }
+
+
+        }
+
+
+         */
+
+        private void toolStripButton_openPMD_Click(object sender, EventArgs e)
+        {
+            using (var fileDialog = new OpenFileDialog())
+            {
+                fileDialog.Title = "Select .pmd file";
+                fileDialog.Filter = "RMG Files (*.pmd)|*.pmd";
+
+                fileDialog.RestoreDirectory = true;
+
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    loadPMD(fileDialog.FileName);
+                }
+            }
+        }
+
+
+    }
+}
